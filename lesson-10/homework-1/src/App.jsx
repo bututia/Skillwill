941a0447a75e710869008056e1afc58b95d295c4
nino <ninojinjo66@gmail.com>
2023-08-03 20:51:47 +0400

    Lesson-10

---
+++ lesson-10/homework-1/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import cat from'./assets/cat.jpg'
+
+function App() {
+  return (
+    <div>
+    <h1>Hi</h1>
+    <div><img src={cat} width="800px" alt="cat"></img></div>
+    </div>
+  );
+}
+
+export default App;
